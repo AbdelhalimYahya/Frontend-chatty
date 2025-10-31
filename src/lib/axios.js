e80abe6c7afa021129992c440bb96c7b175ee178
abdelhalim <abdelhalim@gmail.com>
2025-10-31 21:22:31 +0200

    alsdj

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://backend-chatty-production.up.railway.app/" : "/api",
+  baseURL: "https://backend-chatty-production.up.railway.app",
   withCredentials: true,
 });
